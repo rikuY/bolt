36a1258b390be2867283e889e6b64a73866b68de
riku yamamoto <koi.711.kn@gmail.com>
2018-07-30 16:50:01 +0900

    bolt 修正

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 (function() {
 
-var open = document.getElementById('open');
-var contact = document.getElementById('contact');
-var contactText = document.getElementById('contactText');
-var head = document.getElementById('head');
-var main = document.getElementById('main');
+  var open = document.getElementById('open');
+  var contact = document.getElementById('contact');
+  var contactText = document.getElementById('contactText');
+  var head = document.getElementById('head');
+  var main = document.getElementById('main');
 
-open.addEventListener('click' ,function() {
-  contact.classList.remove('hidden');
-  head.classList.add('bottom');
-  contactText.classList.remove('hidden');
-  main.classList.add('main__margin');
-  })
+  open.addEventListener('click' ,function() {
+    contact.classList.remove('hidden');
+    head.classList.add('bottom');
+    contactText.classList.remove('hidden');
+    main.classList.add('main__margin');
+    })
 })();
