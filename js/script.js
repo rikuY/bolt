928124c770adee28fb24b4c614ffbdc8e5d559a6
riku yamamoto <koi.711.kn@gmail.com>
2018-07-30 15:53:03 +0900

    volto 修正

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-(function () {
-  var bar = document.getElementById('bar');
-  var menu = document.getElementById('menu');
+(function() {
 
-  bar.addEventListener('click' ,function() {
-  menu.classList.remove('hidden');
-  bar.classList.remove('bar')
-  });
+var open = document.getElementById('open');
+var contact = document.getElementById('contact');
+var contactText = document.getElementById('contactText');
+var head = document.getElementById('head');
+var main = document.getElementById('main');
+
+open.addEventListener('click' ,function() {
+  contact.classList.remove('hidden');
+  head.classList.add('bottom');
+  contactText.classList.remove('hidden');
+  main.classList.add('main__margin');
+  })
 })();
